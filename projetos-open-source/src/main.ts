0a3156ce7bb3be78c743c76a95a9da77261a40a1
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-21 12:05:11 -0400

    refactor: enhance HTML structure and improve card rendering

--- projetos-open-source/src/main.ts
+++ projetos-open-source/src/main.ts
@@ -8,8 +8,12 @@ async function carregarCards() {
   const cards: Card[] = await response.json()
 
   app.innerHTML = `
-  <h1>Projetos Open Source</h1>
-  <div id="cards-container" class="cards-container"></div>`
+  <h1 class="titulo">Projetos Open Source</h1>
+  <div>
+    <input type="text" placeholder="Buscar projetos..." class="buscar">
+  </div>
+  <div id="cards-container" class="cards-container"></div>
+  `
 
   const cardsContainer = document.querySelector<HTMLDivElement>("#cards-container")!
 
@@ -19,10 +23,10 @@ async function carregarCards() {
 
     cardDiv.innerHTML = `
     <div class="imagem-card" style="background-color: ${card.cor};"><img src="/${card.icone}"></div>
-    <div class="titulo-card">${card.titulo}</div>
+    <div class="titulo-card"><h3>${card.titulo}</h3></div>
     <div class="descricao-card">${card.descricao}</div>
     <div class="tags-card">
-      ${card.tecnologias.map(tecnologia => `<div class="tag-card">${tecnologia}</div>`)}
+      ${card.tecnologias.map(tecnologia =>`<div class="tag-card">${tecnologia}</div>`).join('')}
     </div>
     <div class="projeto-card"><span>Ver projeto</span></div>
     `
